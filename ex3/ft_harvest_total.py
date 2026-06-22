a696810ac0d8ca0e0d3036494323cc0886db0cc1
root <root@DESKTOP-63BM6S7.>
2026-06-22 18:33:09 +0400

    from ex0-ex5 done!

---
+++ ex3/ft_harvest_total.py
@@ -0,0 +1,7 @@
+def ft_harvest_total():
+    h1 = int(input("Day 1 harvest:"))
+    h2 = int(input("Day 2 harvest:"))
+    h3 = int(input("Day 3 harvest:"))
+    total = h1 + h2 + h3
+    print("Total harvest:",total)
+ft_harvest_total()
---
+++ ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,3 @@
+def ft_count_harvest_iterative():
+    day = int(input("Days until harvest:"))
+    for 
